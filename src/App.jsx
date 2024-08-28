c0d6d5083f172123ab1d2280da9ecf093a51489c
Luis Delgado <dev.ledg555@gmail.com>
2024-08-28 10:37:52 -0600

    Create function to determine the winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
 import { useState } from "react";
 import Cell from "./components/Cell";
 import Turn from "./components/Turn";
-
-const TURNS = {
-  x: "×",
-  o: "o",
-};
+import { computeWinner } from "./utils/functions";
 
 export default function App() {
-  const [board, setBoard] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+  const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(true);
+  const winnerSets = [
+    [board[0], board[1], board[2]],
+    [board[3], board[4], board[5]],
+    [board[6], board[7], board[8]],
+    [board[0], board[3], board[6]],
+    [board[1], board[4], board[7]],
+    [board[2], board[5], board[8]],
+    [board[0], board[4], board[8]],
+    [board[2], board[4], board[6]],
+  ];
 
+  function handleCellClick(index) {
+    if (!board[index]) {
+      let nextBoard = [...board];
+      nextBoard[index] = turn ? "o" : "×";
+      setBoard(nextBoard);
+      setTurn(!turn);
+    }
+  }
+
+  let winner = computeWinner(winnerSets);
+  
   return (
     <main className="flex justify-center w-full">
       <h1>Gato</h1>
       <section className="grid grid-cols-3">
         {board.map((e, i) => (
-          <Cell key={i}>{board[i]}</Cell>
+          <Cell key={i} onClick={() => handleCellClick(i)}>
+            {board[i]}
+          </Cell>
         ))}
       </section>
       <section>
         <Turn form="o" isYourTurn={turn === true}></Turn>
         <Turn form="×" isYourTurn={turn === false}></Turn>
+      </section>
+      <section>
+        {winner && <p>Player {winner} wins!!</p>}
       </section>
     </main>
   );
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,6 +1,6 @@
-export default function Cell({children,}) {
+export default function Cell({children, onClick}) {
   return (
-    <div className="w-16 h-16 border border-yellow-100 text-center">
+    <div className="w-16 h-16 border border-yellow-100 text-center" onClick={onClick}>
       <span className="text-xl">{children}</span>
     </div>
   );
