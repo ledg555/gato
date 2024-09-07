b946a70f8613326637c1c5e0c5c57b15236d9433
Luis Delgado <dev.ledg555@gmail.com>
2024-09-07 12:06:11 -0600

    Make working reset buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import PlayersBar from "@/components/PlayersBar";
 import Scores from "@/components/Scores";
 import Board from "@/components/Board";
 import VictoryMessage from "./components/VictoryMessage";
+import ResetBar from "./components/ResetBar";
 import Turn from "@/components/Turn";
-import { computeWinner, computeScores } from "@/lib/handlers";
-import { initialBoard } from "@/lib/utils";
+import { initialBoard, computeWinner, computeScores } from "@/lib/utils";
 
 export default function App() {
   const [board, setBoard] = useState(initialBoard);
@@ -36,6 +36,22 @@ export default function App() {
     }
   }
 
+  function handleResetBoard() {
+    const clearedBoard = [...initialBoard];
+    setBoard(clearedBoard);
+    setWinner(0);
+    setTurn(opener);
+  }
+
+  function handleResetScores() {
+    const clearedBoard = [...initialBoard];
+    const clearedScores = [0,0];
+    setBoard(clearedBoard);
+    setScores(clearedScores);
+    setWinner(0);
+    setTurn(opener);
+  }
+
   return (
     <div className="bg-slate-300">
       <main className="grid grid-cols-1 justify-items-center w-96 mx-auto bg-blue-200">
@@ -53,6 +69,10 @@ export default function App() {
         </section>
         <VictoryMessage players={players} winner={winner} />
       </main>
+      <footer>
+        <ResetBar board={board} winner={winner} scores={scores} resetBoard={handleResetBoard} resetScores={handleResetScores}></ResetBar>
+        <p className="text-center">Made by <a href="/">ledg555</a></p>
+      </footer>
     </div>
   );
 }
--- src/components/VictoryMessage.jsx
+++ src/components/VictoryMessage.jsx
@@ -2,11 +2,10 @@ export default function VictoryMessage({ players, winner }) {
   let formattedPlayer = "";
   switch (winner) {
     case 0:
-      return <p>hola</p>;
+      return <h2>hola</h2>;
     case 1:
     case 2:
       formattedPlayer = players[winner - 1];
-      console.log(formattedPlayer);
       formattedPlayer.charAt(formattedPlayer.length - 1) === "s"
         ? (formattedPlayer += "'")
         : (formattedPlayer += "'s");
